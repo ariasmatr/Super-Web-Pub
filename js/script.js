4c189c77c5909e6f3fc2cb9421d21d47ab3d2c64
Elena Zaratiegui <ezaratieguigarcia@gmail.com>
2025-01-05 18:15:16 +0100

    delete modal

--- js/script.js
+++ js/script.js
@@ -10,37 +10,6 @@ document.getElementById('switchMode').addEventListener('click',()=>{
     }
   });
 
-  $(document).ready(function () {
-    // Al hacer clic en una imagen
-    $(".img-gallery").on("click", function (e) {
-      e.preventDefault();
-  
-      // Limpiar carrusel anterior
-      const carouselInner = $("#galleryCarousel .carousel-inner");
-      carouselInner.empty();
-  
-      // Obtener todas las imágenes de la sección actual
-      const gallerySection = $(this).closest(".row").find(".img-gallery img");
-  
-      // Crear los slides del carrusel
-      gallerySection.each(function (index) {
-        const src = $(this).attr("src");
-        const alt = $(this).attr("alt");
-        const isActive = index === 0 ? "active" : "";
-  
-        const carouselItem = `
-          <div class="carousel-item ${isActive}">
-            <img src="${src}" class="d-block w-100" alt="${alt}">
-          </div>
-        `;
-  
-        carouselInner.append(carouselItem);
-      });
-  
-      // Abrir el modal
-      $("#galleryModal").modal("show");
-    });
-  });
-  
+
   
   
