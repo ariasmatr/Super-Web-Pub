079f9b25ac1c5daf1cd12598a6ea093b38393e0d
alejandrarobaina <alerobainauni@gmail.com>
2025-01-06 23:43:25 +0100

    error on scroll image

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ const decreaseBtn = document.getElementById('decrease-btn');
             let quantity = parseInt(quantityElement.textContent);
             quantityElement.textContent = quantity + 1;
         });
+
+        window.addEventListener('scroll', function() {
+            const image = document.querySelector('.project-image-hero img');
+            const scrollPosition = window.scrollY;
+            
+            // Modificar la escala de la imagen en función del scroll
+            const scale = 1 - scrollPosition / 1000; // Ajusta el divisor según lo necesario
+            
+            if (image) {
+                image.style.transform = `scale(${scale})`;
+            }
+        });
+
+        
+        
