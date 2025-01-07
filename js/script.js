0bcdb93f712faac5ae5822de449ff84ab4082fb2
Elena Zaratiegui <ezaratieguigarcia@gmail.com>
2025-01-07 20:13:47 +0100

    add Modal 2 y 3

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ console.log("me mato, cosorro");
 
   console.log("necesito ayuda");
 
+  
   document.querySelectorAll('.img-gallery-1').forEach((img) => {
     img.addEventListener('click', function () {
       const slideTo = this.getAttribute('data-slide-to');
@@ -19,4 +20,12 @@ console.log("me mato, cosorro");
       carouselInstance.to(slideTo);
     });
   });
-
+  
+  document.querySelectorAll('.img-gallery-3').forEach((img) => {
+    img.addEventListener('click', function () {
+      const slideTo = this.getAttribute('data-slide-to');
+      const carousel = document.querySelector('#carouselExampleIndicators3');
+      const carouselInstance = bootstrap.Carousel.getInstance(carousel) || new bootstrap.Carousel(carousel);
+      carouselInstance.to(slideTo);
+    });
+  });
