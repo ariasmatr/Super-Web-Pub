3044f9bdb7b40bd5a8ce28eb822baf29007bfffd
Elena Zaratiegui <ezaratieguigarcia@gmail.com>
2025-01-09 22:08:22 +0100

    arreglar gallery

--- js/script-gallery.js
+++ js/script-gallery.js
@@ -2,16 +2,6 @@ console.log("me mato, cosorro");
 
   console.log("necesito ayuda");
 
-  
-  document.querySelectorAll('.img-gallery-1').forEach((img) => {
-    img.addEventListener('click', function () {
-      const slideTo = this.getAttribute('data-slide-to');
-      const carousel = document.querySelector('#carouselExampleIndicators');
-      const carouselInstance = bootstrap.Carousel.getInstance(carousel) || new bootstrap.Carousel(carousel);
-      carouselInstance.to(slideTo);
-    });
-  });
-  
   document.querySelectorAll('.img-gallery-2').forEach((img) => {
     img.addEventListener('click', function () {
       const slideTo = this.getAttribute('data-slide-to');
@@ -21,11 +11,22 @@ console.log("me mato, cosorro");
     });
   });
   
-  document.querySelectorAll('.img-gallery-3').forEach((img) => {
+  
+document.querySelectorAll('.img-gallery-1').forEach((img) => {
+    img.addEventListener('click', function () {
+      const slideTo = this.getAttribute('data-slide-to');
+      const carousel = document.querySelector('#carouselExampleIndicators1');
+      const carouselInstance = bootstrap.Carousel.getInstance(carousel) || new bootstrap.Carousel(carousel);
+      carouselInstance.to(slideTo);
+    });
+  }); 
+
+  
+document.querySelectorAll('.img-gallery-3').forEach((img) => {
     img.addEventListener('click', function () {
       const slideTo = this.getAttribute('data-slide-to');
       const carousel = document.querySelector('#carouselExampleIndicators3');
       const carouselInstance = bootstrap.Carousel.getInstance(carousel) || new bootstrap.Carousel(carousel);
       carouselInstance.to(slideTo);
     });
-  });
+  }); 
