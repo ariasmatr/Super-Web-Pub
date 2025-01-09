c1019b4678acf76a1b938be6449ccf2ec9862c7c
DavidValeiro <davidvaleiro@gmail.com>
2025-01-09 18:53:27 +0100

    Menu y LS

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-console.log("me mato, cosorro");
-
-/* LIGHT/DARK MODE SEGÚN NAVEGADOR Y SE GUARDA */
-function getInitialTheme() {
-    const savedTheme = localStorage.getItem('data-bs-theme');
-    if (savedTheme) {
-        return savedTheme;
+    /* LIGHT/DARK MODE SEGÚN NAVEGADOR Y SE GUARDA */
+    function getInitialTheme() {
+        const savedTheme = localStorage.getItem('data-bs-theme');
+        if (savedTheme) {
+            return savedTheme;
+        }
+        if (window.matchMedia) {
+            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            return prefersDarkScheme ? 'dark' : 'light';
+        }
+        return 'light';
     }
-    if (window.matchMedia) {
-        const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        return prefersDarkScheme ? 'dark' : 'light';
+    function updateIcon(theme) {
+        const switchModeIcon = document.getElementById('switchMode');
+        if (theme === 'dark') {
+            switchModeIcon.classList.remove('bi-brightness-low-fill','fs-3');
+            switchModeIcon.classList.add('bi-moon-fill','fs-4');
+        } else {
+            switchModeIcon.classList.remove('bi-moon-fill','fs-4');
+            switchModeIcon.classList.add('bi-brightness-low-fill','fs-3');
+        }
     }
-    return 'light';
-}
-function updateIcon(theme) {
-    const switchModeIcon = document.getElementById('switchMode');
-    if (theme === 'dark') {
-        switchModeIcon.classList.remove('bi-brightness-low-fill','fs-3');
-        switchModeIcon.classList.add('bi-moon-fill','fs-4');
-    } else {
-        switchModeIcon.classList.remove('bi-moon-fill','fs-4');
-        switchModeIcon.classList.add('bi-brightness-low-fill','fs-3');
-    }
-}
-document.addEventListener('DOMContentLoaded', () => {
-    const initialTheme = getInitialTheme();
-    document.documentElement.setAttribute('data-bs-theme', initialTheme);
-    updateIcon(initialTheme);
-});
-document.getElementById('switchMode').addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-bs-theme', newTheme);
-    localStorage.setItem('data-bs-theme', newTheme);
-    updateIcon(newTheme);
-});
+    document.addEventListener('DOMContentLoaded', () => {
+        const initialTheme = getInitialTheme();
+        document.documentElement.setAttribute('data-bs-theme', initialTheme);
+        updateIcon(initialTheme);
+        loadingScreen()
+    });
+    document.getElementById('switchMode').addEventListener('click', () => {
+        const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('data-bs-theme', newTheme);
+        updateIcon(newTheme);
+        loadingScreen()
+    });
 
 
   /* NAV BUTTON */
@@ -66,12 +66,81 @@ document.getElementById('switchMode').addEventListener('click', () => {
                         item.classList.remove("animate");
                         item.classList.add("menuItem");
                 });
-            }, 200);
+            }, 400);
         }
         }
     });
     
-   
+    
+    function loadingScreen() {
+        function startLoader() {
+            let counterElement = document.querySelector(".count p");
+            let currentValue = 0;
+            function updateCounter() {
+                if (currentValue < 100) {
+                    let increment = Math.floor(Math.random() * 10) + 1;
+                    currentValue = Math.min(currentValue + increment, 100);
+                    counterElement.textContent = currentValue;
+    
+                    let delay = Math.floor(Math.random() * 200) + 25;
+                    setTimeout(updateCounter, delay);
+                }
+            }
+            updateCounter();
+        }
+        gsap.set(".count", { opacity: 1 });
+        gsap.set(".pre-loader", { scale: 1 });
+        gsap.set(".loader", { height: "100%" });
+        gsap.set(".loader-bg", { height: "100%" });
+        startLoader();
+        gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.25 });
+        let textWrapper = document.querySelector('.ml16');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        anime.timeline({ loop: false })
+            .add({
+                targets: '.ml16 .letter',
+                translateY: [-100, 0],
+                easing: "easeOutExpo",
+                duration: 1500,
+                delay: (el, i) => 30 * i
+            })
+            .add({
+                targets: '.ml16 .letter',
+                translateY: [0, 100],
+                easing: "easeOutExpo",
+                duration: 3000,
+                delay: (el, i) => 2000 + 30 * i
+            });
+        gsap.to(".count", {
+            opacity: 0,
+            ease: "power2.inOut",
+            duration: 0.5,
+            delay: 3.75,
+        });
+        gsap.to(".pre-loader", {
+            scale: 0.5,
+            ease: "power4.inOut",
+            duration: 2,
+            delay: 3,
+        });
+        gsap.to(".loader", {
+            height: "0",
+            ease: "power4.inOut",
+            duration: 1.5,
+            delay: 3.75,
+        });
+        gsap.to(".loader-bg", {
+            height: "0",
+            ease: "power4.inOut",
+            duration: 1.5,
+            delay: 4,
+        });
+
+    }
+
+    
+
+
     
 
 
